b14b5e40fc6da0c5648d2b475824d461096b230e
Balazs Nyiro <diogenesz@pergamen.hu>
2022-02-07 21:07:40 -0500

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jyp
+
+go 1.17
---
+++ jyp.go
@@ -0,0 +1,11 @@
+package jyp
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+}
+
+func json_parse(src string) int {
+  fmt.Println("json_parse:" + "hello world")
+  return len(src)
+}
