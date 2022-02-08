25b844da008da0073669afb947dbde24d4e8860a
Balazs Nyiro <diogenesz@pergamen.hu>
2022-02-08 06:51:40 -0500

    before use object forever

--- jyp.go
+++ jyp.go
@@ -10,7 +10,13 @@ type value struct {
 	val_array  []value
 }
 
+func obj() map[string]value {
+	obj_empty := make(map[string]value)
+	return obj_empty
+}
+
 func Json_parse(src string) (int, error) {
 	fmt.Println("json_parse:" + src)
+	// obj := obj()
 	return len(src), nil
 }
