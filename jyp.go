de3f9fa6ead160d477b8181ccdda3db9be8e926c
Balazs Nyiro <diogenesz@pergamen.hu>
2022-02-09 23:21:03 -0500

    split src to runes

--- jyp.go
+++ jyp.go
@@ -11,6 +11,11 @@ type value struct {
 	val_array        []value
 }
 
+type key_value_pair struct {
+	key   string
+	value value
+}
+
 func obj_empty() map[string]value {
 	obj_empty := make(map[string]value)
 	return obj_empty
@@ -21,5 +26,22 @@ func Json_parse(src string) (map[string]value, error) {
 	obj := obj_empty()
 	val := value{val_type: "number_int", val_number_int: 1}
 	obj["key"] = val
+
+	var chars = make([]rune, len(src))
+	for i, rune := range src {
+		fmt.Println(i, "->", string(rune))
+		chars[i] = rune
+	}
+
 	return obj, nil
 }
+
+/*
+func Json_recursive_object_finder(src string) (value, error) {
+	fmt.Println("recursive object finder:" + src)
+	for i, c := range src {
+		fmt.Println(i, " => ", string(c))
+	}
+}
+
+*/
