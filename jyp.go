655f82321e8cd9a8f1ac1c76d5c056366775de9c
Balazs Nyiro <diogenesz@pergamen.hu>
2022-02-10 22:44:56 -0500

    use elems

--- jyp.go
+++ jyp.go
@@ -2,46 +2,45 @@ package jyp
 
 import "fmt"
 
-type value struct {
-	val_type         string // string, number_int, number_float, object, array, true, false, null
+type elem struct {
+	val_type string
+	// rune, string, number_int, number_float,
+	// object, array, true, false, null
+
+	val_rune         rune
 	val_string       string
 	val_number_int   int
 	val_number_float float64
-	val_object       map[string]value
-	val_array        []value
+	val_object       map[string]elem
+	val_array        []elem
 }
 
-type key_value_pair struct {
-	key   string
-	value value
-}
-
-func obj_empty() map[string]value {
-	obj_empty := make(map[string]value)
+func obj_empty() map[string]elem {
+	obj_empty := make(map[string]elem)
 	return obj_empty
 }
 
-func Json_parse(src string) (map[string]value, error) {
+func Json_parse(src string) (map[string]elem, error) {
 	fmt.Println("json_parse:" + src)
 	obj := obj_empty()
-	val := value{val_type: "number_int", val_number_int: 1}
+	val := elem{val_type: "number_int", val_number_int: 1}
 	obj["key"] = val
 
-	var chars = make([]rune, len(src))
+	var chars = make([]elem, len(src))
 	for i, rune := range src {
 		fmt.Println(i, "->", string(rune))
-		chars[i] = rune
+		chars[i] = elem{val_rune: rune, val_type: "rune"}
 	}
+	Json_recursive_object_finder(chars)
 
 	return obj, nil
 }
 
-/*
-func Json_recursive_object_finder(src string) (value, error) {
-	fmt.Println("recursive object finder:" + src)
-	for i, c := range src {
-		fmt.Println(i, " => ", string(c))
+func Json_recursive_object_finder(src []elem) (elem, error) {
+	for i, elem := range src {
+		if elem.val_type == "rune" {
+			fmt.Println(i, " => ", elem.val_type, string(elem.val_rune))
+		}
 	}
+	return src[0], nil
 }
-
-*/
--- jyp_test.go
+++ jyp_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestObjKey(t *testing.T) {
 	object_root, err := Json_parse(`{"key": 1}`)
 
-	val_wanted := value{val_type: "number_int", val_number_int: 1}
+	val_wanted := elem{val_type: "number_int", val_number_int: 1}
 	result_check(object_root["key"], err, val_wanted, nil, t)
 }
 
-func result_check(value_received value, err error, value_wanted value, err_wanted error, t *testing.T) {
+func result_check(value_received elem, err error, value_wanted elem, err_wanted error, t *testing.T) {
 	if value_received.val_number_int != value_wanted.val_number_int || err != err_wanted {
 		t.Fatalf(`ret = %v, %v, want "", error`, value_received, err)
 	}
