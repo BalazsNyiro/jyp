1a5d3f55a06241df37565b9a6a6861f799775f91
Balazs Nyiro <diogenesz@pergamen.hu>
2024-03-01 22:27:52 -0500

    string_detection

---
+++ jyp/jyp.go
@@ -0,0 +1,146 @@
+// jyp - Balazs' Json parser
+// author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
+
+// this file is the implementation of the _standard_ json data format:
+// https://www.json.org/json-en.html
+
+package jyp
+
+import "errors"
+
+const ABC_lower string = "abcdefghijklmnopqrstuvwxyz"
+const ABC_upper string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const digitZero string = "0"
+const digits_19 string = "123456789"
+const digits_09 string = "0123456789"
+
+const whitespaces string = "\r\n\t "
+
+const objOpen  string = "{"
+const arrClose string = "}"
+const arrOpen  string = "["
+const objClose string = "]"
+
+const separatorComma string = ","
+const separatorColon string = ":"
+const separatorDot   string = "."
+const separatorExponent string = "eE"
+
+const separatorMinus     string = "-"
+const separatorPlusMinus string = "+-"
+
+
+
+type Elems []Elem
+type ElemMap map[string]Elem
+
+type Elem struct {
+	ValType string
+	// possible types:
+	// array, object,
+	// bool, null, string, number_int, number_float,
+
+	ValArray       Elems
+	ValObject      ElemMap
+
+	ValBool        bool // true, false
+	isNull		   bool // if true, then the value is null
+
+	ValString      string
+	ValNumberInt   int
+	ValNumberFloat float64
+
+	//////// PARSING SECTION: detection from the JSON source code /////
+	charPositionFirstInSourceCode int   // 0: the first char in source code, 1: 2nd...
+	charPositionLastInSourceCode  int   // 0: the first char in source code, 1: 2nd...
+	runes []rune
+}
+
+type tokenTable_startPositionIndexed map[int]Elem
+
+// if the src can be parsed, return with the JSON root object with nested elems, and err is nil.
+func JsonParse(src string) (Elem, error) {
+	elemRoot := Elem{}
+
+	errorsCollected := []error{}
+	tokens := tokenTable_startPositionIndexed{}
+
+	// a simple rule - inputs:  src, tokens, errors are inputs,
+	//                 outputs: src, tokens, errors
+	// the src is always less and less, as tokens are detected
+	// the tokens table has more and more elems, as the src sections are parsed
+	// at the end, src is total empty (if everything goes well) - and we don't have errors, too
+	src, tokens, errorsCollected = json_string_detect(src, tokens, errorsCollected)
+
+	return elemRoot, nil
+}
+
+
+////////////////////// BASE FUNCTIONS ///////////////////////////////////////////////
+func json_string_detect(src string, tokensStartPositions tokenTable_startPositionIndexed, errorsCollected []error) (string, tokenTable_startPositionIndexed, []error) {
+
+	srcDetectedTokensRemoved := []rune{}
+	// to find escaped \" \\\" sections in strings
+	escapeBackSlashCounterBeforeCurrentChar := 0
+
+	inStringDetection := false
+
+	isEscaped := func() bool {
+		return escapeBackSlashCounterBeforeCurrentChar % 2 != 0
+	}
+
+	var tokenNow Elem
+
+	for posInSrc, r := range src {
+
+		if r == '"' {
+			if !inStringDetection {
+					tokenNow = Elem{}
+					inStringDetection = true
+					tokenNow.charPositionFirstInSourceCode = posInSrc
+					tokenNow.runes = append(tokenNow.runes, r)
+					srcDetectedTokensRemoved = append(srcDetectedTokensRemoved, ' ')
+					continue
+			} else { // in string detection
+				if ! isEscaped() {
+					inStringDetection = false
+					tokenNow.charPositionLastInSourceCode = posInSrc
+					tokenNow.runes = append(tokenNow.runes, r)
+					tokensStartPositions[tokenNow.charPositionFirstInSourceCode] = tokenNow
+					srcDetectedTokensRemoved = append(srcDetectedTokensRemoved, ' ')
+					continue
+				}
+			}
+		} // if " is detected, everything is handled in the conditions
+
+
+		if inStringDetection {
+			tokenNow.runes = append(tokenNow.runes, r)
+
+			if r == '\\' {
+				escapeBackSlashCounterBeforeCurrentChar++
+			} else { // the escape series ended :-)
+				escapeBackSlashCounterBeforeCurrentChar = 0
+			}
+
+			// add empty placeholder where the token was detected
+			srcDetectedTokensRemoved = append(srcDetectedTokensRemoved, ' ')
+		} else {
+			// save the original rune, if it was not in a string
+			srcDetectedTokensRemoved = append(srcDetectedTokensRemoved, r)
+		}
+
+	} // for
+
+	if inStringDetection {
+		errorsCollected = append(errorsCollected, errors.New("non-closed string detected:"))
+	}
+
+	return string(srcDetectedTokensRemoved), tokensStartPositions, errorsCollected
+}
+
+
+
+
+/////////////////////// base functions /////////////////
---
+++ jyp/jyp_test.go
@@ -0,0 +1,89 @@
+
+package jyp
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_detect_strings(t *testing.T) {
+	funName := "Test_detect_strings"
+
+
+	testName := funName + "_emptyString"
+	src := `{"empty":""}`
+	tokensStartPositions := tokenTable_startPositionIndexed{}
+	errorsCollected := []error{}
+
+
+	srcE, tokensE, errorsCollectedE := json_string_detect(src, tokensStartPositions, errorsCollected)
+	// after token detection, the parsed section is removed;
+	//                                       `{"empty":""}`, t)
+	compare_string_string(testName, `{       :  }`, srcE, t)
+
+	compare_int_int(testName, len(tokensE), 2, t)  // 3 strings were detected
+	compare_int_int(testName, len(errorsCollectedE), 0, t)
+	compare_runes_runes(testName, []rune(`"empty"`), tokensE[1].runes, t)
+
+
+
+	testName = funName + "_simpleStringDetect"
+	src = `{"name":"Bob", "age": 42}`
+	tokensStartPositions = tokenTable_startPositionIndexed{}
+	errorsCollected = []error{}
+
+
+	// in tokens2, tokens are indexed by the first char where they were detected
+	src2, tokens2, errorsCollected2 := json_string_detect(src, tokensStartPositions, errorsCollected)
+	//                                       `{"name":"Bob", "age": 42}`
+	// after token detection, the parsed section is removed;
+	compare_string_string(testName, `{      :     ,      : 42}`, src2, t)
+
+	compare_int_int(testName, 3, len(tokens2), t)  // 3 strings were detected
+	compare_int_int(testName, 1, tokens2[1].charPositionFirstInSourceCode,  t)
+	compare_int_int(testName, 6, tokens2[1].charPositionLastInSourceCode, t)
+	compare_runes_runes(testName, []rune(`"name"`), tokens2[1].runes, t)
+
+	compare_int_int(testName, len(errorsCollected2), 0, t)
+}
+
+//////////////////////////// TEST BASE FUNCS ///////////////////
+func compare_int_int(testName string, wantedNum int, received int, t *testing.T) {
+	if wantedNum != received {
+		t.Fatalf("\nError in %s wanted: %d, received: %d", testName, wantedNum, received)
+	}
+}
+
+func compare_bool_bool(testName string, wanted bool, received bool, t *testing.T) {
+	if wanted != received {
+		t.Fatalf("\nError, different bool comparison %s wanted: %t, received: %t", testName, wanted, received)
+	}
+}
+
+func compare_string_string(callerInfo, strWanted, strReceived string, t *testing.T) {
+	if strWanted != strReceived {
+		t.Fatalf("\nErr String difference (%s):\n  wanted -->>%s<<-- ??\nreceived -->>%s<<--\n\n", callerInfo, strWanted, strReceived)
+	}
+}
+
+func compare_runes_runes(callerInfo string, runesWanted, runesReceived []rune, t *testing.T) {
+	errMsg := fmt.Sprintf("\nErr (%s) []rune <>[]rune:\n  wanted -->>%s<<-- ??\nreceived -->>%s<<--\n\n", callerInfo, string(runesWanted), string(runesReceived))
+	if len(runesWanted) != len(runesReceived) {
+		t.Fatalf(errMsg)
+		return
+	}
+
+	for pos, runeWanted:= range runesWanted {
+		if runeWanted != runesReceived[pos] {
+			t.Fatalf(errMsg)
+			return
+		}
+	}
+}
+
+func compare_rune_rune(callerInfo string, runeWanted, runeReceived rune, t *testing.T) {
+	if runeWanted != runeReceived {
+		errMsg := fmt.Sprintf("\nErr (%s) rune <>rune:\n  wanted -->>%s<<-- ??\nreceived -->>%s<<--\n\n", callerInfo, string(runeWanted), string(runeReceived))
+		t.Fatalf(errMsg)
+	}
+}
