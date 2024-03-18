bcc787a712af78090bd119b43704a9222315ce53
Balazs Nyiro <diogenesz@pergamen.hu>
2024-03-17 21:36:34 -0400

    tokens

--- jyp/jyp_B.go
+++ jyp/jyp_B.go
@@ -41,16 +41,12 @@ func tokensTableDetect_versionB(srcStr string) tokenElems{
 	posUnknownBlockStart := -1 // used only if the token is longer than 1 char. numbers, false/true for example
 	
 	//////////// TOKEN ADD func ///////////////////////
-	tokenAddedInForLoop := ""
 	tokenAdd := func (typeOfToken rune, posFirst, posLast int) {
-		tokenAddedInForLoop = ""         // the unknown blocks can be added IF a know block follows them.
 		if posUnknownBlockStart != -1 {  // JSON has to be in containers {...} or [...] so it is closed with a known elem
 			tokenTable = append(tokenTable, tokenElem_B{tokenType: '?', posInSrcFirst: posUnknownBlockStart, posInSrcLast: posFirst-1}  )
 			posUnknownBlockStart = -1
-			tokenAddedInForLoop += "?"
 		}
 		tokenTable = append(tokenTable, tokenElem_B{tokenType: typeOfToken, posInSrcFirst: posFirst, posInSrcLast: posLast}  )
-		tokenAddedInForLoop += string(typeOfToken)
 	} // func, tokenAdd //////////////////////////////
 	
 	
@@ -58,23 +54,26 @@ func tokensTableDetect_versionB(srcStr string) tokenElems{
 	inString := func () bool { // if string start position detected,
 		return posStringStart != -1    // we are in String detection
 	} //////////////////////////////////////////////////////////////
-
+	/*
+	inStringDebug := func(runeNow rune) string {
+		info := " "
+		if inString() || runeNow == '"' { info = "S" } // display S for debugging, when inString==true
+		return info
+	}
+	 */
 	isEscaped := false
 
 	for pos, runeNow := range srcStr {
-
-		tokenAddedInForLoop = "---" // updated from tokenAdd
-
 
 		stringCloseAtEnd := false
 		if runeNow == '"' {
 			if ! inString() {
 				posStringStart = pos // posStringStart is modified only if interval is started
 			} else { // in string processing:
-				if ! isEscaped {
-					stringCloseAtEnd = true // string can be closed only at the end of the codeBlock, not here.
-				}
-			}
+				if ! isEscaped { // and not escaped:
+					stringCloseAtEnd = true
+				} // string can be closed only at the end of the codeBlock, not here.
+			}     // so the closing " is part of the string section
 		} /////////////////////////////////////
 
 		// detect tokens:
@@ -82,23 +81,14 @@ func tokensTableDetect_versionB(srcStr string) tokenElems{
 			if runeNow == '{' || runeNow == '}' || runeNow == '[' || runeNow == ']' || runeNow == ',' || runeNow == ':' {
 				tokenAdd(runeNow, pos, pos)
 			} else {
-
 				// not in string, and not json structural char
 				// skip whitespaces:
-				if ! base__is_whitespace_rune(runeNow){
-					posUnknownBlockStart = pos
+				if ! base__is_whitespace_rune(runeNow) && posUnknownBlockStart == -1 {
+					posUnknownBlockStart = pos // save block start
 					// standard Json has to be closed with known
 				}
-
-
-			} // wide token maybe
-
-
+			} // unknown token
 		} // not inString
-
-
-
-
 
 
 		///////////////////// CLOSING administration ////////////////
@@ -110,19 +100,13 @@ func tokensTableDetect_versionB(srcStr string) tokenElems{
 			}
 		}
 
-		inStringInfo := " " // administration
-		if inString() {
-			inStringInfo = "S"
-			tokenAddedInForLoop = "\"" // this will be added in a stringCloseAtEnd
-		}
-
 		if stringCloseAtEnd {
 			tokenAdd('"', posStringStart, pos)
 			posStringStart = -1
 		}
+		// fmt.Println(fmt.Sprintf("pos: %2d", pos), string(runeNow), inStringDebug(runeNow), " token:", tokenAddedInForLoop)
 
-		fmt.Println(fmt.Sprintf("pos: %2d", pos), string(runeNow), inStringInfo, " token:", tokenAddedInForLoop)
-	}
+	} // for, tokenTable
 	return tokenTable
 }
 
@@ -140,4 +124,14 @@ func base__is_whitespace_rune(oneRune rune) bool { // TESTED
 		Other definitions of spacing characters are set by category Z and property Pattern_White_Space.
 	*/
 	return unicode.IsSpace(oneRune)
+}
+
+func base__print_tokenElems(tokenElems tokenElems) {
+	for _, tokenElem := range tokenElems {
+		fmt.Println(
+			string(tokenElem.tokenType),
+			fmt.Sprintf("%2d", tokenElem.posInSrcFirst),
+			fmt.Sprintf("%2d", tokenElem.posInSrcLast),
+			)
+	}
 }
--- jyp/jyp_B_test.go
+++ jyp/jyp_B_test.go
@@ -11,16 +11,26 @@ LICENSE file in the root directory of this source tree.
 
 package jyp
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 //  go test -v -run  Test_tokensTableDetect_versionB
 func Test_tokensTableDetect_versionB(t *testing.T) {
 	funName := "Test_tokensTableDetect_versionB"
 	testName := funName + "_basic"
 
-	src := `{"text":{"level2":[321,4.5,"string\"Escaped",true,false,null]}}`
+	//src := `{"text":{"level2":[321,4.5,"string\"Escaped",true,false,null]}}`
+	src := file_read_to_string("large-file.json")
 
+	timeStart := time.Now()
 	tokensTableB := tokensTableDetect_versionB(src)
+	fmt.Println("token table creation time:", time.Since(timeStart))
+
 	_ = tokensTableB
 	_ = testName
+
+	// base__print_tokenElems(tokensTableB)
 }
